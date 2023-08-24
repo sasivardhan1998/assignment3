b64838ccaa1a3ca37840b84e26f774599761433c
sasivardhan1998 <sasivardhan.marolix@gmail.com>
2023-08-24 19:13:00 +0530

    initial commit

--- combine two dictionary by adding values for common keys.py
+++
@@ -1,6 +0,0 @@
-
-from collections import Counter  
-dict1 = {'a': 100, 'b': 200, 'c':300}  
-dict2 = {'a': 300, 'b': 200, 'd':400}  
-new_dict=Counter(dict1)+Counter(dict2)
-print("new dict:",new_dict)
