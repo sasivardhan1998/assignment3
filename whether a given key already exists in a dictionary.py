ea86ab1d2d50343d51a1acbfc6aad17094fbe14c
sasivardhan1998 <sasivardhan.marolix@gmail.com>
2023-08-24 18:01:14 +0530

    initial commit

---
+++ whether a given key already exists in a dictionary.py
@@ -0,0 +1,8 @@
+dict={"name":"prabha","emp_id":"mt-0001","salary":"20000"}
+if "emp_id" in dict:
+        print("yes,'emp_id' is one of the key in dict")
+
+dict={"name": "phabha","age":"25","rollno":"30"}
+if "age" in dict:
+        print("yes, 'age' is one of the key in dict")
+        
