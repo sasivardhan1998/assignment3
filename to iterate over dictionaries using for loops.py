336af0ec87630402e8d35385c56994166be6d4d7
sasivardhan1998 <sasivardhan.marolix@gmail.com>
2023-08-24 18:07:08 +0530

    initial commit

---
+++ to iterate over dictionaries using for loops.py
@@ -0,0 +1,9 @@
+dict = {'a': 'juice', 'b': 'grill', 'c': 'corn'}
+for key,value in  dict.items():
+    print(key,value)
+
+
+
+dict = {'a': 'billa', 'b': 'grill', 'c': 'aata'}
+for key,value in  dict.items():
+       print(key,value)
